cb1d43a75dad3e5c4d8782166f03d54b1de5a6cd
enzomazer <enzokmitani@gmail.com>
2024-03-22 09:17:37 -0300

    tentei

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
             <header>
-                <h1>cabeçalho</h1>
+                <h1>telefone</h1>
             </header>
     );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
-export default function Main(){
-    const [nome, setNome] = useState("");
-
-    return(
-            <main>
-                    <input
-                    type="text"
-                    name=""
-                    id=""
-                    onChange={(event)=> setNome(event.target.value)}
-                    />
-                    
-                    {nome}
-            </main>
-    );
-}
+export default function telefone(){
+        const [tel, setTel] = useState("");
+    
+        return(
+                <main>
+                        <form>
+                                <p>nome:</p>
+                                <input type="tel" name="a" id="tele"/> 
+                                <p>telefone:</p>
+                                <input type="tel" name="" id="nome"/>
+                                <br></br>
+                                <button onClick={(event)=> setTel(document.getElementById("tele").value +": "+ document.getElementById("nome").value)}>enviar</button>
+                        </form>
+                        <label>{tel}</label>
+                </main>
+        );
+    }
